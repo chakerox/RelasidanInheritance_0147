3b18522a6240700278327548366622d665e92f9b
chakerox <chakerox@gmail.com>
2026-05-12 13:09:19 +0700

    membuat ibu dan anak baru

--- agregasi.cpp
+++ agregasi.cpp
@@ -4,3 +4,8 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
+ ibu* varIbu = new ibu("Siti");
+    ibu* varIbu2 = new ibu("Sarah");
+    anak* varAnak1 = new anak("Budi");
+    anak* varAnak2 = new anak("Dewi");
+    anak* varAnak3 = new anak("Andi");
