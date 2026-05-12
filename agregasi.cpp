727d0ac7d0b9728367d12cfda22645418ae85d6b
chakerox <chakerox@gmail.com>
2026-05-12 13:10:05 +0700

    menambahkan anak

--- agregasi.cpp
+++ agregasi.cpp
@@ -4,8 +4,13 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
- ibu* varIbu = new ibu("Siti");
+    ibu* varIbu = new ibu("Siti");
     ibu* varIbu2 = new ibu("Sarah");
     anak* varAnak1 = new anak("Budi");
     anak* varAnak2 = new anak("Dewi");
     anak* varAnak3 = new anak("Andi");
+
+    varIbu->tambahkanAnak(varAnak1);
+    varIbu->tambahkanAnak(varAnak2);
+    varIbu2->tambahkanAnak(varAnak3);
+    varIbu2->tambahkanAnak(varAnak1);
